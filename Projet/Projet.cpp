71817feccfaa0f73db8b5a9c99ef3606bb949271
Saibe1111 <56834108+Saibe1111@users.noreply.github.com>
2019-10-24 09:57:25 +0200

    SPRINT 4

--- Projet/Projet.cpp
+++ Projet/Projet.cpp
@@ -10,6 +10,7 @@
 #define nbPatEqu 3 // Le nombre de patineurs par équipe
 #define nbEquEpr 2 // Le nombre d’équipes par épreuve
 
+
 typedef struct {
 	char nom[lgMot + 1];
 	int dossard;
@@ -30,7 +31,7 @@ typedef struct {
 }Epreuve;
 
 typedef struct {
-	float temps[11];
+	float temps[13];
 	int tour;
 	int dossard;
 }temp;
@@ -41,7 +42,11 @@ typedef struct {
 }Mesure;
 typedef struct {
 	int parcours;
-	int max[20]; // stock temps max chaque equipe
+	int Poursuites[32]; // epreuve
+	int fin[16];
+	int var;
+	float tempEqui[nbEquEpr * maxEpreuves];
+	char Equi[nbEquEpr * maxEpreuves];
 }Parcours;
 
 void inscription_equipe(Inscrits* insc, int* dossard, int nbEquInsc, Mesure* mesu) {
@@ -56,8 +61,11 @@ void inscription_equipe(Inscrits* insc, int* dossard, int nbEquInsc, Mesure* mes
 		strcpy(insc->list[nbEquInsc].data[i].nom, mot);
 		insc->list[nbEquInsc].data[i].dossard = *dossard + i;
 		printf("inscription dossard %i\n", *dossard + i);
+		//mesu->doss[*dossard + i].temps[12] = 0;
+		//printf("Pour dossard: %i", *dossard + i);
+		//printf(" temps initialise a: %i \n", mesu->doss[*dossard + i].temps[12]);
 		d = *dossard + i;
-		for (int i = 1; i < 11; ++i) {
+		for (int i = 1; i < 13; ++i) {
 			mesu->doss[d].tour = i;
 			mesu->doss[d].temps[mesu->doss[d].tour] = 0;
 			//printf(" enregistrement temps tour:%i \n Dossard:%i \n temps: %i \n", mesu->doss[d].tour, d, mesu->doss[d].temps[mesu->doss[d].tour]);
@@ -65,7 +73,8 @@ void inscription_equipe(Inscrits* insc, int* dossard, int nbEquInsc, Mesure* mes
 	}
 
 
-}void detection_fin_poursuite(Mesure* mesu, Parcours* parc, int nbEquInsc, const Inscrits* ins, int tour);
+}
+void detection_fin_poursuite(Mesure* mesu, Parcours* parc, int nbEquInsc, const Inscrits* ins, int tour);
 
 void affichage_equipe(const Inscrits* ins, int nbEquInsc) {
 
@@ -89,6 +98,17 @@ void enregistrer_temps(Mesure* mesu, int nbEqInsc, Parcours* p, Inscrits* ins) {
 	mesu->doss[d].tour = e;
 	scanf("%f", tmp);
 	mesu->doss[d].temps[mesu->doss[d].tour] = *tmp;
+
+
+	// enregistrement best temps 
+	//printf(" temps a questioner: %3.1f \n", mesu->doss[d].temps[e]);
+	//printf("%3.1f \n", mesu->doss[d].temps[12]);
+	if (mesu->doss[d].temps[mesu->doss[d].tour] > mesu->doss[d].temps[12]) {
+		//printf("Remplacement meilleur temps !!");
+		mesu->doss[d].temps[12] = mesu->doss[d].temps[e];
+		//printf(" temps apres prog: %3.1f \n", mesu->doss[d].temps[12]);
+
+	}
 	detection_fin_poursuite(mesu, p, nbEqInsc, ins, e);
 }
 void afficher_temps(const Mesure* mesu, const Inscrits* ins, int nbEquInsc) {
@@ -142,24 +162,120 @@ void definition_parcours(Parcours* parc) {
 		printf("Impossible \n");
 }
 void detection_fin_poursuite(Mesure* mesu, Parcours* parc, int nbEquInsc, const Inscrits* ins, int tour) {
+
 	int p = parc->parcours;
 	int dmax = nbEquInsc * 3 + 100;
 	int dInt = 0;
 	int EqInsc = nbEquInsc;
 	for (int l = 1; l < (nbEquInsc / 2) + 1; ++l) {
-		int k = 0;
+		parc->Poursuites[l] = 0;
 		int d = 101;
 		dInt = d + l * 6;
-		for (int i = 1; i < p + 1; ++i) {
+		//printf("L:%i \n", l);
+		for (int i = 1; i < 7; ++i) {
 			for (int j = dInt - 6; j < dInt; ++j) {
-				if (mesu->doss[j].temps[i] != 0) {
-					k++;
+				if (mesu->doss[j].temps[i] > 0) {
+					//printf("%3.1f \n", mesu->doss[j].temps[i]);
+					//printf("%d \n", j);
+					parc->Poursuites[l] = parc->Poursuites[l] + 1;
+					//printf("%i \n", parc->Poursuites[l]);
 				}
 			}
 		}
-		if (p == (k / 6)) {
-			printf("dection_fin_poursuite \n");
+		if (p == (parc->Poursuites[l] / 6) && parc->fin[l] == 0) {
+			printf("detection_fin_poursuite \n");
+			parc->fin[l] = 1;
+			int dossa = 6 * l + 95; //nbEquEpr * nbPatEqu = 6
+			int t = 12;
+			int j = parc->var;
+			//printf("%i", parc->var);
+			//for (j; j < nbEquInsc; j++) {
+			int E1;
+			int E2;
+			float tmp1;
+			float tmp2;
+
+			if (mesu->doss[dossa + 1].temps[t] < mesu->doss[dossa].temps[t] && mesu->doss[dossa + 2].temps[t] < mesu->doss[dossa].temps[t]) {
+				//printf("%s ", ins->list[j].pays);
+				//parc->Equi[parc->var] = j;
+				E1 = j;
+				//("%3.1f \n", mesu->doss[dossa].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa].temps[t];
+				tmp1 = mesu->doss[dossa].temps[t];
+				parc->var = parc->var + 1;
+			}
+			if (mesu->doss[dossa].temps[t] < mesu->doss[dossa + 1].temps[t] && mesu->doss[dossa + 2].temps[t] < mesu->doss[dossa + 1].temps[t]) {
+				//printf("%s ", ins->list[j].pays);
+				//parc->Equi[parc->var] = j;
+				E1 = j;
+				//printf("%3.1f \n", mesu->doss[dossa + 1].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa + 1].temps[t];
+				tmp1 = mesu->doss[dossa + 1].temps[t];
+				parc->var = parc->var + 1;
+			}
+			if (mesu->doss[dossa + 1].temps[t] < mesu->doss[dossa + 2].temps[t] && mesu->doss[dossa].temps[t] < mesu->doss[dossa + 2].temps[t]) {
+				//printf("%s ", ins->list[j].pays);
+				E1 = j;
+				//parc->Equi[parc->var] = j;
+				//printf("%3.1f \n", mesu->doss[dossa + 2].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa + 2].temps[t];
+				tmp1 = mesu->doss[dossa + 2].temps[t];
+				parc->var = parc->var + 1;
+			}
+			dossa = dossa + 3;
+
+			if (mesu->doss[dossa + 1].temps[t] < mesu->doss[dossa].temps[t] && mesu->doss[dossa + 2].temps[t] < mesu->doss[dossa].temps[t]) {
+				//printf("%s ", ins->list[j + 1].pays);
+				E2 = j + 1;
+				//parc->Equi[parc->var] = j + 1;
+				//printf("%3.1f \n", mesu->doss[dossa].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa].temps[t];
+				tmp2 = mesu->doss[dossa].temps[t];
+				parc->var = parc->var + 1;
+			}
+			if (mesu->doss[dossa].temps[t] < mesu->doss[dossa + 1].temps[t] && mesu->doss[dossa + 2].temps[t] < mesu->doss[dossa + 1].temps[t]) {
+				//printf("%s ", ins->list[j+1].pays);
+				//parc->Equi[parc->var] = j+1;
+				E2 = j + 1;
+				//printf("%3.1f \n", mesu->doss[dossa + 1].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa + 1 ].temps[t];
+				tmp2 = mesu->doss[dossa + 1].temps[t];
+				parc->var = parc->var + 1;
+			}
+			if (mesu->doss[dossa + 1].temps[t] < mesu->doss[dossa + 2].temps[t] && mesu->doss[dossa].temps[t] < mesu->doss[dossa + 2].temps[t]) {
+				//printf("%s ", ins->list[j+1].pays);
+				//parc->Equi[parc->var] = j+1;
+				E2 = j + 1;
+				//printf("%3.1f \n", mesu->doss[dossa + 2].temps[t]);
+				//parc->tempEqui[parc->var] = mesu->doss[dossa + 2].temps[t];
+				tmp2 = mesu->doss[dossa + 2].temps[t];
+				parc->var = parc->var + 1;
+			}
+			dossa = dossa + 3;
+
+			//printf("%i\n",dossa);
+			if (tmp2 > tmp1) {
+
+				printf("%s ", ins->list[E1].pays);
+				printf("%3.1f \n", tmp1);
+				printf("%s ", ins->list[E2].pays);
+				printf("%3.1f \n", tmp2);
+			}
+			else
+			{
+
+				printf("%s ", ins->list[E2].pays);
+				printf("%3.1f \n", tmp2);
+				printf("%s ", ins->list[E1].pays);
+				printf("%3.1f \n", tmp1);
+
+
+			}
+			//}
+		//dossa = dossa + 3;
 		}
+
+
 	}
 }
 
@@ -169,8 +285,22 @@ int main() {
 	Mesure m;
 	Inscrits i;
 	Parcours p;
+	p.var = 0;
 	char mot[lgMot + 1];
 	i.nbIns = 0;
+
+	for (int i = 0; i < 10; ++i) {
+		p.fin[i] = 0;
+	}
+	// -> remplace 8 lignes
+	//p.fin[1] = 0;
+	//p.fin[2] = 0;
+	//p.fin[3] = 0;
+	//p.fin[4] = 0;
+	//p.fin[5] = 0;
+	//p.fin[6] = 0;
+	//p.fin[7] = 0;
+	//p.fin[8] = 0;
 	while (1) {
 		// Lecture de la commande (mot) 
 		scanf("%s", mot);
@@ -203,7 +333,6 @@ int main() {
 		}
 		// si la commande est "exit". 
 		if (strcmp(mot, "exit") == 0) {
-			printf("Sortie du programme principal...");
 			exit(0);
 			// sortie du programme principal 
 		}
